446b7a0b64251948741d054e868689fda77db85a
niepeng <2472059195@qq.com>
2019-06-11 19:10:10 +0800

    miaoshu

--- manager/src/index.js
+++ manager/src/index.js
@@ -8,7 +8,7 @@ const app = dva();
 // app.use({});
 
 // 3. Model
-app.model(require('./models/user').default);
+// app.model(require('./models/example').default);
 
 // 4. Router
 app.router(require('./router').default);
