74888497f828826863f277c582af9259d4742df9
niepeng <2472059195@qq.com>
2019-06-11 13:40:36 +0800

    miaoshu

--- manager/src/router.js
+++ manager/src/router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import Login from './views/login/login';
-import mainPage from './views/main/mainPage';
+import MainPage from './views/main/MainPage';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={Login} />
-        <Route path="/main" exact component={mainPage} />
+        <Route path="/main" exact component={MainPage} />
       </Switch>
     </Router>
   );
