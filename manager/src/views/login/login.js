49c2ad9cceeb766173fee52e478c2cd1b0484fe1
niepeng <2472059195@qq.com>
2019-06-11 20:02:26 +0800

    zuoye

--- manager/src/views/login/login.js
+++ manager/src/views/login/login.js
@@ -1,140 +1,97 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import { connect } from 'dva';
-import { Link } from 'dva/router'
 import styles from './login.css'
 
-// class Login extends Component {
+function LoginPage(props){
 
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             userName: '',
-//             userPwd: '',
-//             nameflag: false,
-//             psdflag: false
-//         }
-//     }
+  // 获取login
+  let {login} = props;
+  useEffect(()=>{
+    login({
+      user_name: 'chenmanjie',
+      user_pwd: 'Chenmanjie123!'
+    })
+  }, []);
 
-//     changeName(e) {
-//         if (e.target.value === 'chenmanjie') {
-//             this.setState({
-//                 userName: e.target.value,
-//                 nameflag: true
-//             })
-//         }
-//     }
-
-//     changePwd(e) {
-//         if (e.target.value === 'chenmanjie123!') {
-//             this.setState({
-//                 userPwd: e.target.value,
-//                 pwdflag: true
-//             })
-//         }
-//     }
-
-//     login() {
-//         // console.log(this)
-//         // if(this.state.nameflag && this.state.pwdflag){
-//         //     console.log(this.props)
-//         //     let {history} = this.props;
-//         //     history.push({
-//         //         pathname:'/home'
-//         //     })
-//         // }
-//     }
-
-//     render() {
-//         let { userName, userPwd } = this.state;
-//         return (
-//             <div className={styles.wrapper}>
-//                 <div className={styles.wrap}>
-//                     <div className={styles.writeIn}>
-//                         <img src={require("../../assets/yonghu.png")} alt="" />
-//                         <input type="text" value={userName} placeholder="请输入用户名" onChange={this.changeName.bind(this)} />
-//                     </div>
-//                     <div className={styles.writeIn}>
-//                         <img src={require("../../assets/mima.png")} alt="" />
-//                         <input type="text" value={userPwd} placeholder="请输入用户密码" onChange={this.changePwd.bind(this)} />
-//                     </div>
-//                     <div className={styles.check}>
-//                         <div>
-//                             <span></span>
-//                             <p>记住密码</p>
-//                         </div>
-//                         <p>忘记密码</p>
-//                     </div>
-//                     <Link className={styles.btn} to="/main">登陆</Link>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-// function Login(props) {
-//     console.log(props);
-//     function changeName(e){
-//         console.log(e)
-//     }
-
-//     function changePwd(){
-//         console.log('222')
-//     }
-
-//     function login(){
-//         console.log(333)
-//     }
-
-
-
-// Login.defaultProps = {
-//     userName:'111',
-//     userPwd:'222'
-// };
-
-// Login.propTypes = {
-
-// };
-
-// export default connect()(Login);
-
-
-
-
-
-
-
-
-
-class LoginPage extends Component {
-    componentDidMount() {
-        let { login } = this.props;
+  // 处理表单提交
+  let handleSubmit = e => {
+    e.preventDefault();
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        // 调登录接口
         login({
-            user_name: "chenmanjie",
-            user_pwd: "Chenmanjie123!"
+          user_name: values.username,
+          user_pwd: values.password
         })
-    }
-    render() {
-        return (
-            <div className={styles.wrapper}>
-                
-            </div>
-        );
-    }
+      }
+    });
+  };
+  // 表单校验
+  const { getFieldDecorator } = props.form;
+  return  <Form onSubmit={handleSubmit} className="login-form">
+    <Form.Item>
+      {getFieldDecorator('username', {
+        validateTrigger: 'onBlur',
+        rules: [{required: true, message: '请输入正确的用户名'}],
+      })(
+        <Input
+          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder="Username"
+        />,
+      )}
+    </Form.Item>
+    <Form.Item>
+      {getFieldDecorator('password', {
+        rules: [{ pattern: /^(?:(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])).*$/, message: '请输入正确的密码' }],
+      })(
+        <Input
+          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          type="password"
+          placeholder="Password"
+        />,
+      )}
+    </Form.Item>
+    <Form.Item>
+      {getFieldDecorator('remember', {
+        valuePropName: 'checked',
+        initialValue: true,
+      })(<Checkbox>Remember me</Checkbox>)}
+      <a className="login-form-forgot" href="">
+        Forgot password
+      </a>
+      <Button type="primary" htmlType="submit" className="login-form-button">
+        Log in
+      </Button>
+      Or <a href="">register now!</a>
+    </Form.Item>
+  </Form>;
+}
+
+// props的类型检查
+LoginPage.propTypes = {
 
 }
-const mapStateToProps = state => {
-    console.log("state...", state);
-    return {}
+// props的默认值
+LoginPage.defaultProps = {
+
 }
-const mapDispatchToProps = dispatch => {
-    return {
-        login(payload) {
-            dispatch({
-                type: 'user/login',
-                payload
-            })
-        }
+
+const mapStateToProps = state=>{
+  console.log('state...', state);
+  return {}
+}
+
+const mapDisaptchToProps = dispatch=>{
+  return {
+    login(payload){
+      dispatch({
+        type: 'user/login',
+        payload
+      })
     }
+  }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(LoginPage)
+
+export default connect(mapStateToProps, mapDisaptchToProps)(Form.create()(LoginPage))
